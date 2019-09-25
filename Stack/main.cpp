62eea9dd3c98bd0501924430c5555727b81c3ce1
JanulkaCorkaFizdejki <nowinski.robert@gmail.com>
2019-09-25 17:58:37 +0200

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Robert Nowiński on 25/09/2019.
+//  Copyright © 2019 Robert Nowiński. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
